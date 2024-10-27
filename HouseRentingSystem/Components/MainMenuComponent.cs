0f7a97edd86c91845191458b59d03019c191791b
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-10-27 16:30:43 +0200

    MainMenuComponent Added

---
+++ HouseRentingSystem/Components/MainMenuComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        /// <summary>
+        /// Component for Main Menu
+        /// </summary>
+        /// <returns>
+        /// View(Default.cshtml)
+        /// View(otherName.cshtml)
+        /// </returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}

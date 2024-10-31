abe41d82e39dfb51561d1e841efb5a4911da3302
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-10-31 21:56:06 +0200

    House Service and HoiseIndexModel Added

---
+++ HouseRentingSystem.Core/Models/Home/HouseIndexServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Core.Models.Home
+{
+    public class HouseIndexServiceModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string ImageUrl { get; set; }
+    }
+}

d234e671e36862b023640fdc90f59b6d3f28ec6e
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-10-31 22:10:34 +0200

    Base Controller Added

---
+++ HouseRentingSystem/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HouseRentingSystem.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}

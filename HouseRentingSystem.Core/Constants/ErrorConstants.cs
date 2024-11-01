727da5866c5894d913589046fd0479fa400d5aae
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-11-01 02:18:54 +0200

    Become Agent Model Created

---
+++ HouseRentingSystem.Core/Constants/ErrorConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Core.Constants
+{
+    public static class ErrorConstants
+    {
+        public const string RequiredMessage = "The {0} field is required.";
+        public const string InvalidLengthMessage = "The {0} must be between {2} and {1} characters long.";
+    }
+}
--- HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+using static HouseRentingSystem.Core.Constants.ErrorConstants;
+
+namespace HouseRentingSystem.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength, 
+            MinimumLength = AgentPhoneNumberMinLength,
+            ErrorMessage = InvalidLengthMessage)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
     }
 }

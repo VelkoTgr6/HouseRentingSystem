dd594452fc7cb409c679c3174a503e6a7d2696b0
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-11-13 18:06:25 +0200

    Added Services

--- HouseRentingSystem.Core/Models/House/AllHousesQueryModel.cs
+++ HouseRentingSystem.Core/Models/House/AllHousesQueryModel.cs
@@ -1,6 +1,11 @@
-﻿namespace HouseRentingSystem.Core.Models.House
+﻿using HouseRentingSystem.Core.Models.Home;
+
+namespace HouseRentingSystem.Core.Models.House
 {
     public class AllHousesQueryModel
     {
+        public int TotalHousesCount { get; set; }
+
+        public IEnumerable<HouseServiceModel> Houses { get; set; } = new List<HouseServiceModel>();
     }
 }
